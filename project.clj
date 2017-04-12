fe4d9d48b380911807c42c207fdea3b0e42dc769
Jesse Dowell <jesse@jcodelabs.com>
2017-04-12 18:07:57 -0400

    basic deps in place, authentication, channel fetching

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.3.0"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/data.zip "0.1.2"]]
   :main ^:skip-aot gitmirth.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/gitmirth/core.clj
+++ src/gitmirth/core.clj
@@ -1,7 +1,51 @@
 (ns gitmirth.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-http.client :as client]
+            [clojure.data.xml :as xml]
+            [clojure.data.zip.xml :as zx]
+            [clojure.zip :as zip]))
+
+(def api-url "https://misc:8443/api")
+
+(defn with-authentication
+  "Binds a cookie store to keep auth cookies, authenticates, and
+  returns the results of executing the supplied fn"
+  [func]
+  (binding [clj-http.core/*cookie-store* (clj-http.cookies/cookie-store)]
+    (client/post
+     (str api-url "/users/_login")
+     {:form-params
+      {:username "admin"
+       :password "admin"}
+      :insecure? true})
+    (func)))
+
+(defn fetch-all-channels-xml
+  "Fetch all channels from remote server"
+  []
+  (:body (client/get (str api-url "/channels") {:insecure? true})))
+
+(defn to-zip
+  "Takes a string of xml and returns an xml zipper"
+  [x]
+  (zip/xml-zip (xml/parse-str x)))
+
+(defn process-channels
+  ""
+  []
+  (let [channel-zips (zx/xml-> (to-zip (fetch-all-channels-xml)) :list :channel)]
+    (map #(zx/xml-> % :name zip/down zip/node) channel-zips)))
+
+(defn foo
+  ""
+  []
+  (process-channels))
+
+(defn dostuff
+  "App logic"
+  []
+  (with-authentication foo))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (dostuff))
