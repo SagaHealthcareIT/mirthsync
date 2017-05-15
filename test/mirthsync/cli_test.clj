97a091379654c94845474df31d5612b6c01c1121
Jesse Dowell <jesse@jcodelabs.com>
2017-05-15 19:39:25 -0400

    shell test files

---
+++ test/mirthsync/cli_test.clj
@@ -0,0 +1,9 @@
+(ns mirthsync.cli-test
+  (:require [mirthsync.cli :refer :all]
+            [clojure.test :refer :all]))
+
+;; (deftest a-test
+;;   (testing "FIXME, I fail."
+;;     (is (= 0 1))))
+
+
