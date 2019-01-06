edb33f771683fe8be10b362966cb957dc4675245
jessesaga <jesse@saga-it.com>
2019-01-06 08:12:07 -0500

    project description edits

--- project.clj
+++ project.clj
@@ -1,11 +1,17 @@
-(defproject com.saga-it/mirthsync "0.1.0"
-  :description "Mirthsync is a command line tool for keeping a local
-  copy of important aspects of Mirth Connect configuration in order to
-  allow for the use of traditional version control tools like Git or
-  SVN. Downloading and uploading to a remote Mirth server are both
-  supported. The only requirements are having credentials for the
-  server that is being synced and the server also needs to support and
-  allow access to the REST API."
+(defproject com.saga-it/mirthsync "1.0.1-SNAPSHOT"
+  :description "Mirthsync is a command line tool, created by Saga IT,
+  for keeping a local copy of important aspects of Mirth Connect
+  configuration in order to allow for the use of traditional version
+  control tools like Git or SVN. With Mirthsync you are able to
+  selectively pull the code for channels, groups, and more from a
+  local or remote Mirth Connect instance and have the code placed into
+  a local hierarchy of files and directories that can be tracked using
+  version control. Selectively pushing code to local or remote Mirth
+  Connect servers is also possible using Mirthsync.
+
+  The only requirements are having credentials for the
+  server that is being synced and ensuring that the server is
+  configured to allow access to its REST API."
   :url "https://github.com/SagaHealthcareIT/mirthsync"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
