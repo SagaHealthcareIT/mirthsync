6364dcb73520fc414dc3878f4777fa573bc6320c
jessesaga <jesse@saga-it.com>
2019-02-04 14:13:23 -0500

    shell scripts

---
+++ pkg/mirthsync.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+
+_dir=`dirname $(readlink -f $0)`
+
+## mostly from - https://stackoverflow.com/a/7335524 goal is to detect
+## java version and potentially disable access warnings
+if type -p java; then
+    echo found java executable in PATH
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo found java executable in JAVA_HOME     
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "no java"
+fi
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo version "$version"
+    # if [[ "$version" > "1.8" ]]; then
+    #     $_java --illegal-access=permit -jar ${_dir}/lib/uberjar/mirthsync-2.0.0-SNAPSHOT-standalone.jar $@
+    # else         
+        $_java -jar ${_dir}/lib/uberjar/mirthsync-2.0.0-SNAPSHOT-standalone.jar $@
+    # fi
+fi
+##
