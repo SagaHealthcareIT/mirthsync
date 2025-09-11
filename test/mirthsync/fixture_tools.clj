26dead1d52d1964d9d8255f045684a1ebb4d2a78
Jesse Dowell <jesse.dowell@gmail.com>
2025-09-11 17:45:52 -0400

    disable older mirth version tests for now

--- test/mirthsync/fixture_tools.clj
+++ test/mirthsync/fixture_tools.clj
@@ -229,7 +229,7 @@
 ;; (def mirth-3-09-fixture (apply mirth-fixture mirth-3-09))
 ;; (def mirth-3-11-fixture (apply mirth-fixture mirth-3-11))
 ;; (def mirth-3-12-fixture (apply mirth-fixture mirth-3-12))
-(def mirth-4-01-fixture (apply mirth-fixture mirth-4-01))
+;; (def mirth-4-01-fixture (apply mirth-fixture mirth-4-01))
 (def oie-4-52-fixture (apply mirth-fixture oie-4-52))
 
 ;;;;;;;;;;;;;;; The following was the original script created for
